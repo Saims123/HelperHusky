27a45bc000bc17bf72c9b9cb0f060ac9631abd53
Steve Sim <stevesim@hotmail.com>
2019-07-10 15:24:55 +0100

    Update fix for deployment #1

--- src/app/services/auth/oauth.ts
+++ src/app/services/auth/oauth.ts
@@ -1,4 +1,4 @@
 export const OAuthSettings = {
   appId: '88f381f9-855a-4f2d-b0f8-835331781141',
-  scopes: ['User.Read', 'openid', 'User.ReadBasic.All', 'Mail.ReadWrite', 'Mail.Send', 'Calendars.ReadWrite', 'calendars.read']
+  scopes: ['profile', 'openid', 'User.ReadBasic.All', 'Mail.ReadWrite', 'Mail.Send', 'Calendars.ReadWrite', 'calendars.read']
 };
